56fd88ad21d278ac5b7d19191ec80fde289c12ec
keigo9 <keigo.matsunari@gmail.com>
2023-07-23 15:41:35 +0900

    add-events

--- add-event.js
+++ add-event.js
@@ -1,7 +1,7 @@
 const { google } = require('googleapis')
 require('dotenv').config()
 
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+const SCOPES = 'https://www.googleapis.com/auth/calendar.events'
 const GOOGLE_PRIVATE_KEY = require('./service-account-key.json').private_key
 const GOOGLE_CLIENT_EMAIL = require('./service-account-key.json').client_email
 const GOOGLE_PROJECT_NUMBER = process.env.GOOGLE_PROJECT_NUMBER
@@ -15,23 +15,40 @@ const calendar = google.calendar({
   auth: jwtClient,
 })
 
+const eventData = {
+  summary: "サマリー",
+  description: "hello",
+  allDayEvent: true,
+  start: {
+    date: "2023-07-23"
+    //dateTime: "2023-05-03T18:03:58+02:00"
+  },
+  end: {
+    // endDate: +1 is needed
+    date: "2023-07-25"
+    //dateTime: "2023-05-03T18:03:58+02:00"
+  },
+  reminders: {
+    useDefault: false,
+    overrides: [
+      { method: 'popup', 'minutes': 540 }
+    ]
+  }
+}
+
 const main = () => {
   // 現在以降の最新10件のイベントを取得
-  calendar.events.list(
+  calendar.events.insert(
     {
       calendarId: GOOGLE_CALENDAR_ID,
-      //timeMin: new Date().toISOString(),
-      //timeMax: new Date().toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
+      resource: eventData,
     },
     (error, result) => {
       if (error) {
         console.log(error)
       } else {
-        if (result.data.items.length) {
-          console.log(result.data.items)
+        if (result.data) {
+          console.log(result.data)
           //console.log(result.data.items.map((item) => console.log(item)))
           //console.log(result.data.items.filter((item) => !item.description?.includes("Pay")))
         } else {
